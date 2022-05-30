ecacc162743e145c24506397f09b1df1c92f5662
Justin <justin.maney@MacBook-Pro.attlocal.net>
2022-05-30 15:06:34 -0500

    adds html panel and basic output panel display

--- scripts.js
+++ scripts.js
@@ -11,3 +11,13 @@ $(".toggleButton").click(function () {
   $(this).toggleClass("active");
   $(this).removeClass("highlightedBtn");
 });
+
+$(".panel").height($(window).height() - $("#header").height() * 2);
+
+$(".panel").width($(window).width() / 2 - 10);
+
+$("iframe").contents().find("html").html($("#htmlPanel").val());
+
+$("textarea").on("change keyup paste", function () {
+  $("iframe").contents().find("html").html($("#htmlPanel").val());
+});
