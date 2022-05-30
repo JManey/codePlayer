ae6e5e14950d56eb7d030789e1e07e00fd42e38e
Justin <justin.maney@MacBook-Pro.attlocal.net>
2022-05-30 15:45:56 -0500

    completes button and panel display elements

--- scripts.js
+++ scripts.js
@@ -10,11 +10,18 @@ $(".toggleButton").hover(
 $(".toggleButton").click(function () {
   $(this).toggleClass("active");
   $(this).removeClass("highlightedBtn");
+
+  let panelID = $(this).attr("id") + "Panel";
+  $("#" + panelID).toggleClass("hidden");
+
+  let numberActivePanels = 4 - $(".hidden").length;
+
+  $(".panel").width($(window).width() / numberActivePanels) - 10;
 });
 
 $(".panel").height($(window).height() - $("#header").height() * 2);
 
-$(".panel").width($(window).width() / 2 - 10);
+$(".panel").width($(window).width() / 2) - 10;
 
 $("iframe").contents().find("html").html($("#htmlPanel").val());
 
