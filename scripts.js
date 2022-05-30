46af0e097e47a283879a50049a8ef81808e8be96
Justin <justin.maney@MacBook-Pro.attlocal.net>
2022-05-30 16:17:15 -0500

    refactor and create updateOutput function

--- scripts.js
+++ scripts.js
@@ -23,8 +23,25 @@ $(".panel").height($(window).height() - $("#header").height() * 2);
 
 $(".panel").width($(window).width() / 2) - 10;
 
-$("iframe").contents().find("html").html($("#htmlPanel").val());
+// updates display on page load
+updateOutput();
 
+//updtes output when textarea changes
 $("textarea").on("change keyup paste", function () {
-  $("iframe").contents().find("html").html($("#htmlPanel").val());
+  updateOutput();
 });
+
+// ***************** functions *******************
+
+function updateOutput() {
+  $("iframe")
+    .contents()
+    .find("html")
+    .html(
+      "<html><head><style type='text/css'>" +
+        $("#cssPanel").val() +
+        "</style></head><body>" +
+        $("#htmlPanel").val() +
+        "</body></html>"
+    );
+}
